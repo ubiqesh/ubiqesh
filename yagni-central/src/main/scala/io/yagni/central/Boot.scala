8ad51a571c4f8693a1e0ecba9e0bcfdcc94edbcb
Christoph Grotz <christophgrotz@gmail.com>
2013-12-23 00:38:31 +0100

    Implemented mock REST-API

--- yagni-central/src/main/scala/io/yagni/central/Boot.scala
+++ yagni-central/src/main/scala/io/yagni/central/Boot.scala
@@ -5,73 +5,17 @@ import org.vertx.scala.core.http.{HttpServerRequest, RouteMatcher}
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
 import spray.can.Http
+import org.vertx.java.core.Handler
 
-object Boot extends App {
-
-  // we need an ActorSystem to host our application in
-  implicit val system = ActorSystem("yagni-central")
-
-  // create and start our service actor
-  val service = system.actorOf(Props[MyServiceActor], "demo-service")
-
-  // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ! Http.Bind(service, interface = "localhost", port = 8080)
-}
-
-/*
 object Boot extends App {
   implicit val system = ActorSystem("central")
-  val service = system.actorOf(Props[Supervisor], "Supervisor")
+  val supervisor = system.actorOf(Props[Supervisor], "Supervisor")
   val vertx = org.vertx.scala.core.newVertx()
   val rm = new RouteMatcher()
-  rm.all("/api/1(.)*", new RestEndpoint())
-  vertx.createHttpServer().requestHandler(rm).listen(8080)
-}*/
-
-class RestEndpoint extends RouteMatcher {
-  get("/api/1", {
-    case req: HttpServerRequest => {
-      val resp = req.response()
-      resp.setStatusCode(204).setStatusMessage("ok").setChunked(true)
-      resp.headers.add("Content-Type", "application/json")
-      resp.write("{'devices': '" + req.uri + "/devices'}").end()
+  rm.all("/v1(.)*", {
+    request:HttpServerRequest => {
+      supervisor.tell(request, supervisor)
     }
   })
-
-  allWithRegEx("/api/1/devices(.)*", new DeviceEndpoint())
-}
-
-class DeviceEndpoint extends RouteMatcher {
-  val devices = Map()
-
-  get("/api/1/devices", {
-    case req: HttpServerRequest => {
-      val resp = req.response()
-      resp.setStatusCode(204).setStatusMessage("ok").setChunked(true)
-      resp.headers.add("Content-Type", "application/json")
-      resp.write(devices.values.mkString).end()
-    }
-  })
-  post("/api/1/devices", {
-    case req: HttpServerRequest => req.response().setStatusCode(202).setStatusMessage("ok").end()
-  })
-  options("/api/1/devices", {
-    case req: HttpServerRequest => req.response().setStatusCode(202).setStatusMessage("ok").end()
-  })
-  get("/api/1/devices/:id", {
-    case req: HttpServerRequest => req.response().setStatusCode(202).setStatusMessage("ok").end()
-  })
-  put("/api/1/devices/:id", {
-    case req: HttpServerRequest => req.response().setStatusCode(202).setStatusMessage("ok").end()
-  })
-  patch("/api/1/devices/:id", {
-    case req: HttpServerRequest => req.response().setStatusCode(202).setStatusMessage("ok").end()
-  })
-  delete("/api/1/devices/:id", {
-    case req: HttpServerRequest => req.response().setStatusCode(202).setStatusMessage("ok").end()
-  })
-  options("/api/1/devices/:id", {
-    case req: HttpServerRequest => req.response().setStatusCode(202).setStatusMessage("ok").end()
-  })
-
+  vertx.createHttpServer().requestHandler(rm).listen(8080, "localhost")
 }
--- yagni-central/src/main/scala/io/yagni/central/Supervisor.scala
+++ yagni-central/src/main/scala/io/yagni/central/Supervisor.scala
@@ -7,8 +7,109 @@ class Supervisor extends UntypedActor with ActorLogging {
   def onReceive(message: Any) = {
     if (message.isInstanceOf[HttpServerRequest]) {
       val req = message.asInstanceOf[HttpServerRequest]
-
-      req.response().end()
+      val serverName = req.absoluteURI().getScheme +"://"+req.absoluteURI().getHost+":"+req.absoluteURI().getPort
+      if(req.uri().endsWith("/v1/garden_temperature"))
+      {
+        req.response()
+          .putHeader("Content-Type","application/json")
+          .end("""{
+             "name": "garden_temperature2",
+             "description": "Temperature of test setup in garden.",
+             "current": """"+serverName+"""/v1/garden_temperature/current",
+             "type": {
+                 "name": """"+serverName+"""/v1/types/number_with_unit",
+                 "unit": {
+                     "system": "SI",
+                     "name": "K"
+                 }
+             },
+             "refresh": {
+                 "timestamp": "2013-12-19T21:59,578+05:30",
+                 "name": """"+serverName+"""/v1/refresh_strategies/interval",
+                 "interval": {
+                     "period": "PT5S",
+                     "likely_update": "2013-12-19T22:04,578+05:30"
+                 }
+             },
+             "range": {
+                 "earliest": "2011-12-01T07:59,4+05:30",
+                 "latest": "2013-12-19T21:59,578+05:30"
+             }
+         }""")
+      }
+      else if(req.uri().endsWith("/v1/garden_temperature/current")) {
+        req.response().putHeader("Content-Type","application/json").end("""{
+              "timestamp": "2013-12-19T21:59,578+05:30",
+              "value": 270.8
+          }""")
+      }
+      else if(req.uri().endsWith("/v1/garden_temperature/subscribe")) {
+        req.response().end()
+      }
+      else if(req.uri().endsWith("/v1/rainfall")) {
+        req.response()
+          .putHeader("Content-Type","application/json")
+          .end("""{
+                "name": "rainfall",
+                "description": "Aggregated data example.",
+                "current": """"+serverName+"""/v1/rainfall/current",
+                "type": {
+                    "name": """"+serverName+"""/v1/types/number_with_unit",
+                    "unit": {
+                        "system": "SI",
+                        "name": "l"
+                    }
+                },
+                "refresh": {
+                    "timestamp": "2013-12-19T21:59,578+05:30",
+                    "aggregation": {
+                        "method": """"+serverName+"""/v1/aggregations/summarize",
+                        "period": "PT-1H"
+                    },
+                    "name": """"+serverName+"""/v1/refresh_strategies/interval",
+                    "interval": {
+                        "period": "PT1H",
+                        "likely_update": "2013-12-19T22:04,578+05:30"
+                    }
+                },
+                "range": {
+                    "earliest": "2011-12-01T07:59,4+05:30",
+                    "latest": "2013-12-19T21:59,578+05:30"
+                }
+            }""")
+      }
+      else if(req.uri().endsWith("/v1/rainfall/current")) {
+        req.response()
+          .putHeader("Content-Type","application/json")
+          .end("""{
+                      "timestamp": "2013-12-19T21:59,578+05:30",
+                      "aggregation": {
+                          "method": """"+serverName+"""/v1/aggregations/summarize",
+                          "period": "PT-1H"
+                      },
+                      "value": 2.4
+                  }""")
+      }
+      else
+      {
+        req.response()
+          .putHeader("Content-Type","application/json")
+          .end("""{
+                  "description": "Weather Station Example",
+                  "sensors": [
+                  {
+                    "name": "garden_temperature",
+                    "description": "Temperature of test setup in garden.",
+                    "uri": """"+serverName+"""/v1/garden_temperature"
+                  },
+                  {
+                    "name": "rainfall",
+                    "description": "Aggregated data example.",
+                    "uri": """"+serverName+"""/v1/rainfall"
+                  }
+                  ]
+                }""");
+      }
     }
   }
 }
